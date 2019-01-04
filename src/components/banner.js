71ac61428786b11d9e8ddd97e8e5e0841be75c24
Tyler Lam <tylerl264@gmail.com>
2019-01-03 17:08:33 -0700

    finish up banner and search bar

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class Banner extends Component {
   render() {
     return (
-      <table className="banner">
-        <img className="center" alt="app icon" width="100" src="logo.jpg" />
+      <div className="banner">
+        <img className="center" alt="app icon" width="25" src="logo.jpg" />
         <h1 className="title">MoviesDB Search Client</h1>
-      </table>
+      </div>
     );
   }
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -8,17 +8,21 @@ export default class SearchBar extends Component {
   }
   render() {
     return (
-      <form className="input-group">
-        <input
-          placeholder="Search for your favorite movies"
-          className="form-control"
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">
-            Search
-          </button>
-        </span>
-      </form>
+      <div className="bac">
+        <div class="col-md-6 col-md-offset-3">
+          <form className="input-group">
+            <input
+              placeholder="Search for your favorite movies"
+              className="form-control"
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-secondary">
+                Search
+              </button>
+            </span>
+          </form>
+        </div>
+      </div>
     );
   }
 }
