c24a37ec474f15bedfec8d312f785b795102a9e5
Tyler Lam <tylerl264@gmail.com>
2019-01-03 18:28:13 -0700

    added state to the search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,16 +4,27 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: " " };
+    this.state = { term: "" };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
   }
   render() {
     return (
       <div className="bac">
-        <div class="col-md-6 col-md-offset-3">
-          <form className="input-group">
+        <div className="col-md-6 col-md-offset-3">
+          <form onSubmit={this.onFormSubmit} className="input-group">
             <input
               placeholder="Search for your favorite movies"
               className="form-control"
+              value={this.state.term}
+              onChange={this.onInputChange}
             />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-secondary">
